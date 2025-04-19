6b17037505d8cc7bd3d926f6e0ead4ac3780683b
Bao Nguyen <102691555+baonguyen1530@users.noreply.github.com>
2025-04-19 01:39:33 -0400

    Modify globals.css (make borders completely black) and other components (button,input,progress,textarea,checkbox)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,28 @@
 import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Progress } from "@/components/ui/progress"
+import { Textarea } from "@/components/ui/textarea"
 
 export default function Home() {
   return(
-    <div>
-      <p className="text-4xl">Hello World</p>
+    <div className="p-4">
+      <div className="flex flex-col gap-y-4">
+        <div>
+          <Button variant="elevated">
+            I am a button
+          </Button>
+        </div>
+        <div>
+          <Input placeholder="I am an input"></Input>
+        </div>
+        <div>
+          <Progress value={50}></Progress>
+        </div>
+        <div>
+          <Textarea placeholder="This is a textarea"></Textarea>
+        </div>
+      </div>
     </div>
+    
   )
 }
