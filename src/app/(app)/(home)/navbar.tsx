40ffd924fa17ecc66ad7a89600d5d6ee3bba825d
Bao Nguyen <102691555+baonguyen1530@users.noreply.github.com>
2025-05-19 13:15:40 -0400

    update

--- src/app/(app)/(home)/navbar.tsx
+++ src/app/(app)/(home)/navbar.tsx
@@ -60,7 +60,7 @@ export const Navbar = () => {
         <nav className="h-20 flex border-b justify-between font-medium bg-white">
             <Link href="/" className="pl-6 flex items-center">
                 <span className={cn("text-3xl font-semibold", poppins.className)}>
-                    4rchived_g4rments
+                    4rchiv3dGarm3nts
                 </span>
             </Link>
 
@@ -90,7 +90,7 @@ export const Navbar = () => {
                     variant="secondary"
                     className="border-l border-t-0 border-b-0 border-r-0 px-12 h-full rounded-none bg-white hover:bg-pink-400 transition-colors text-lg"
                 >
-                    <Link href="/sign-in">
+                    <Link prefetch href="/sign-in">
                         Log in
                     </Link>
                 </Button>
@@ -100,7 +100,7 @@ export const Navbar = () => {
                     variant="secondary"
                     className="border-l border-t-0 border-b-0 border-r-0 px-12 h-full rounded-none bg-black hover:bg-pink-400 hover:text-black transition-colors text-lg text-white"
                 >
-                    <Link href="/sign-up">
+                    <Link prefetch href="/sign-up">
                         Start selling
                     </Link>
                 </Button>
---
+++ src/modules/auth/constants.ts
@@ -0,0 +1,1 @@
+export const AUTH_COOKIE = "ecomm-payload-token";
