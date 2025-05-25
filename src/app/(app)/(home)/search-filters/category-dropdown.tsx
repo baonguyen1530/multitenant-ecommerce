70653f37824f1a9f5a02a74efcee84c0e5bedada
Bao Nguyen <102691555+baonguyen1530@users.noreply.github.com>
2025-05-25 20:12:54 +0200

    add products collection

---
+++ src/app/(app)/(home)/search-filters/category-dropdown.tsx
@@ -0,0 +1,85 @@
+"use client";
+
+import { useRef, useState } from "react";
+
+
+import { Button } from "@/components/ui/button"
+import { cn } from "@/lib/utils"
+import { useDropDownPosition } from "./use-dropdown-position"
+import { SubcategoryMenu } from "./subcategory-menu"
+import Link from "next/link";
+import { CategoriesGetManyOutput } from "@/modules/categories/types";
+
+
+interface Props {
+    category: CategoriesGetManyOutput[1];
+    isActive?: boolean;
+    isNavigationHovered?: boolean;
+};
+
+export const CategoryDropdown = ({
+    category,
+    isActive,
+    isNavigationHovered,
+}: Props) => {
+    const [isOpen, setIsOpen] = useState(false);
+    const dropdownRef = useRef<HTMLDivElement>(null);
+    const {getDropdownPosition} = useDropDownPosition(dropdownRef);
+
+    const onMouseEnter = () => {
+        if(category.subcategories) {
+            setIsOpen(true);
+        }
+    }
+
+    const onMouseLeave = () => setIsOpen(false);
+
+    const dropdownPosition = getDropdownPosition();
+
+    // const toggleDropdown = () => {
+    //     if (category.subcategories?.docs?.length) {
+    //         setIsOpen(!isOpen);
+    //     }
+    // };
+
+    return (
+        <div
+            className="relative"
+            ref={dropdownRef}
+            onMouseEnter={onMouseEnter}
+            onMouseLeave={onMouseLeave}
+            // onClick={toggleDropdown}
+        >
+            <div className="relative">
+                <Button
+                    variant = "elevated"
+                    className = {cn(
+                        "h-11 px-4 bg-transparent border-transparent rounded-full hover:bg-white hover:border-primary text-black",
+                        isActive && !isNavigationHovered && "bg-white-primary",
+                        isOpen && "bg-white border-primary shadow-[3px_2px_0px_0px_rgba(0,0,0,1)] hover:-translate-x-[4px]"
+                    )}
+                >
+                    <Link
+                        href={`/${category.slug === "all" ? "" : category.slug}`}
+                    >
+                        {category.name}
+                    </Link>
+                </Button>
+                {category.subcategories && category.subcategories.length > 0 && (
+                    <div
+                        className={cn(
+                            "opacity-0 absolute -bottom-3 w-0 h-0 border-l-[10px] border-r-[10px] border-b-[10px] border-l-transparent border-r-transparent border-b-black left-1/2 -translate-x-1/2",
+                            isOpen && "opacity-100"
+                        )}
+                    />
+                )}
+            </div>
+
+            <SubcategoryMenu
+                category={category}
+                isOpen={isOpen}
+                position={dropdownPosition}
+            />
+        </div>
+    );
+};
---
+++ src/app/(app)/(home)/search-filters/search-input.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { ListFilter, SearchIcon } from "lucide-react";
+import { Input } from "@/components/ui/input";
+import { CategoriesSidebar } from "./categories-sidebar";
+import { useState } from "react";
+import { Button } from "@/components/ui/button";
+
+interface Props {
+    disabled?: boolean;
+};
+
+export const SearchInput = ({
+    disabled,
+}: Props) => {
+    const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+    return(
+        <div className="flex items-center gap-2 w-full">
+            <CategoriesSidebar open={isSidebarOpen} onOpenChange={setIsSidebarOpen}/>
+            <div className="relative w-full">
+                <SearchIcon className="absolute left-3 top-1/2 -translate-y-1/2 size-4 text-neutral-500"></SearchIcon>
+                <Input className="pl-8" placeholder="Search products" disabled={disabled}></Input>
+            </div>
+            {/* TODO: Add categories view all button */}
+            <Button
+                variant="elevated"
+                className="size-12 shrink-0 flex lg:hidden"
+                onClick={() => setIsSidebarOpen(true)}
+            >
+                <ListFilter></ListFilter>
+            </Button>
+            {/* TODO: Add library button */}
+        </div>
+    );
+};
