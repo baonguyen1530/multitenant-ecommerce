5710072ddda0e375a4c1c4b887a9a72e402a2733
Bao Nguyen <102691555+baonguyen1530@users.noreply.github.com>
2025-06-13 19:03:39 -0400

    create getOne product procedure, create product-view.txs component, modify product-card.tsx link href

--- src/modules/tenants/ui/components/footer.tsx
+++ src/modules/tenants/ui/components/footer.tsx
@@ -14,7 +14,7 @@ export const Footer = () => {
             <div className="max-w-(--breakpoint-xl) mx-auto flex gap-2 items-center h-full px-4 py-6 lg:px-12">
                 <p>Powered by</p>
                 <Link href="/">
-                    <span className={cn("text-2xl font-semibold", poppins.className)}>
+                    <span className={cn("text-xl font-semibold", poppins.className)}>
                         4rchiv3dGarm3nts
                     </span>
                 </Link>
