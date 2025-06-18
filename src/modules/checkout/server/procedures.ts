ac1f3341ff12c0d02f749f77f7147d4a19e38968
Bao Nguyen <102691555+baonguyen1530@users.noreply.github.com>
2025-06-18 19:30:48 -0400

    update

--- src/modules/checkout/server/procedures.ts
+++ src/modules/checkout/server/procedures.ts
@@ -1,10 +1,81 @@
 import z from "zod";
-import { baseProcedure, createTRPCRouter } from "@/trpc/init";
+import { baseProcedure, createTRPCRouter, protectedProcedure } from "@/trpc/init";
 import { Media, Tenant } from "@/payload-types";
 import { TRPCError } from "@trpc/server";
+import Stripe from "stripe";
 
 
 export const checkoutRouter = createTRPCRouter({
+    purchase: protectedProcedure
+        .input(
+            z.object({
+                productIds: z.array(z.string()).min(1),
+                tenantSlug: z.string().min(1),
+            })
+        )
+        .mutation(async ({ ctx, input }) => {
+            const products = await ctx.db.find({
+                collection: "products",
+                depth: 2,
+                where: {
+                    and: [
+                        {
+                            id: {
+                                in: input.productIds,
+                            }
+                        },
+                        {
+                            "tenant.slug": {
+                                equals: input.tenantSlug
+                            }
+                        }
+                    ]
+                }
+            })
+
+            if (products.totalDocs !== input.productIds.length) {
+                throw new TRPCError({ code: "NOT_FOUND", message: "Products not found" });
+            }
+
+            const tenantsData = await ctx.db.find({
+                collection: "tenants",
+                limit: 1,
+                pagination: false,
+                where: {
+                    slug: {
+                        equals: input.tenantSlug,
+                    },
+                },
+            });
+
+            const tenant = tenantsData.docs[0];
+
+            if (!tenant) {
+                throw new TRPCError({
+                    code: "NOT_FOUND",
+                    message: "Tenant not found"
+                });
+            };
+
+            const lineItems: Stripe.Checkout.SessionCreateParams.LineItem[] =
+                products.docs.map((product) => ({
+                    quantity: 1,
+                    price_data: {
+                        unit_amount: product.price * 100, // Stripe handles prices in cents
+                        currency: "usd",
+                        product_data: {
+                            name: product.name,
+                            metadata: {
+                                stripeAccountId: tenant.stripeAccountId,
+                                id: product.id,
+                                name: product.name,
+                                price: product.price
+                            }
+                        }
+                    }
+                }))
+        })
+    ,
     getProducts: baseProcedure
         .input(
             z.object({
---
+++ src/modules/checkout/types.ts
@@ -0,0 +1,6 @@
+export type ProductMetadata = {
+    stripeAccountId: string;
+    id: string;
+    name: string;
+    price: number;
+};
