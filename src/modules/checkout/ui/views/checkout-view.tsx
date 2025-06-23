d42d914e904682461fa846b3fc4112c11e5accdf
Bao Nguyen <102691555+baonguyen1530@users.noreply.github.com>
2025-06-23 17:01:18 -0400

    update

--- src/modules/checkout/ui/views/checkout-view.tsx
+++ src/modules/checkout/ui/views/checkout-view.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { useTRPC } from "@/trpc/client";
-import { useMutation, useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { useCart } from "../../hooks/use-cart";
 import { useEffect } from "react";
 import { toast } from "sonner";
@@ -20,7 +20,7 @@ export const CheckoutView = ({ tenantSlug }: CheckoutViewProps) => {
     const router = useRouter();
     const [states, setStates] = useCheckoutStates();
     const { productIds, removeProduct, clearCart } = useCart(tenantSlug);
-    
+    const queryClient = useQueryClient();
     const trpc = useTRPC();
     const { data, error, isLoading } = useQuery(trpc.checkout.getProducts.queryOptions({
         ids: productIds,
@@ -47,10 +47,17 @@ export const CheckoutView = ({ tenantSlug }: CheckoutViewProps) => {
         if (states.success) {
             setStates({ success: false, cancel: false });
             clearCart();
-            // TODO: Invalidate library
+            queryClient.invalidateQueries(trpc.library.getMany.infiniteQueryFilter());
             router.push("/library");
         }
-    }, [states.success, clearCart, router, setStates]);
+    }, [
+        states.success, 
+        clearCart, 
+        router, 
+        setStates,
+        queryClient,
+        trpc.library.getMany,
+    ]);
 
     useEffect(() => {
         if (error?.data?.code === "NOT_FOUND") {
