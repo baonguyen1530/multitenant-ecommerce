de09c598ef196570120ed811817b54c3f50070a9
Bao Nguyen <baowao3015872@gmail.com>
2025-07-01 14:36:59 -0400

    Update page.tsx

--- src/app/(app)/(tenants)/stripe-verify/page.tsx
+++ src/app/(app)/(tenants)/stripe-verify/page.tsx
@@ -2,18 +2,29 @@
 
 import { useTRPC } from "@/trpc/client";
 import { useMutation } from "@tanstack/react-query";
+import { LoaderIcon } from "lucide-react";
+import { useEffect } from "react";
 
 const Page = () => {
     const trpc = useTRPC();
-    const { mutate: verify } = useMutation(trpc.checkout.verify.mutationOptions());
+    const { mutate: verify } = useMutation(trpc.checkout.verify.mutationOptions({
+        onSuccess: (data) => {
+            console.log("Success data:", data);
+            window.location.href = data.url;
+        },
+        onError: (error) => {
+            console.log("Verification error:", error);
+            window.location.href = "/";
+        },
+    }));
 
     useEffect(() => {
-        
-    })
+        verify();
+    }, [verify]);
 
     return (
-        <div>
-            Verification
+        <div className="flex min-h-screen items-center justify-center">
+            <LoaderIcon className="animate-spin text-muted-foreground"/>
         </div>
     );
 }
