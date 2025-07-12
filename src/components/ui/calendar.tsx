980704b9801799418a2a33352365ed9f3dc120f9
Bao Nguyen <102691555+baonguyen1530@users.noreply.github.com>
2025-07-12 14:51:05 -0400

    update before deploying

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -60,12 +60,10 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        IconLeft: ({ className, ...props }) => (
-          <ChevronLeft className={cn("size-4", className)} {...props} />
-        ),
-        IconRight: ({ className, ...props }) => (
-          <ChevronRight className={cn("size-4", className)} {...props} />
-        ),
+        Chevron: ({ orientation, className, ...props }) => {
+          const ChevronIcon = orientation === "left" ? ChevronLeft : ChevronRight;
+          return <ChevronIcon className={cn("size-4", className)} {...props} />;
+        },
       }}
       {...props}
     />
